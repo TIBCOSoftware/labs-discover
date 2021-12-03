43aa465246f92b085d5fbda26071b38d79e13765
hpeters@tibco.com <hpeters@tibco.com>
2021-12-03 11:59:59 +0100

    Removed node_modules

--- public-api/microservices/node_modules/@types/node-cache/README.md
+++
@@ -1,3 +0,0 @@
-This is a stub types definition for node-cache (https://github.com/mpneuried/nodecache).
-
-node-cache provides its own type definitions, so you don't need @types/node-cache installed!
--- public-api/microservices/node_modules/routing-controllers/esm2015/util/isPromiseLike.js
+++
@@ -1,7 +0,0 @@
-/**
- * Checks if given value is a Promise-like object.
- */
-export function isPromiseLike(arg) {
-    return arg != null && typeof arg === 'object' && typeof arg.then === 'function';
-}
-//# sourceMappingURL=isPromiseLike.js.map
